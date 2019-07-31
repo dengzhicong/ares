426f7adeac239e8b79bfab6eec7a8bf2061207a9
admin <admin@DESKTOP-V6VU9L6>
2019-07-31 10:44:22 +0800

    更新json工具

--- ares_utils/src/main/java/com/ares/json/JsonUtils.java
+++ ares_utils/src/main/java/com/ares/json/JsonUtils.java
@@ -1,6 +1,7 @@
 package com.ares.json;
 
 import java.util.Collection;
+import java.util.List;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -14,12 +15,42 @@ import com.ares.log.LogUtil;
  * @author Noah
  *
  */
-public class JSONUtil {
+public class JsonUtils {
+
+//	public static void main(String[] args) {
+//		User user = new User();
+//		user.setAge(12);
+//		user.setMan(true);
+//		user.setName("Hel");
+//
+//		User user2 = new User();
+//		user2.setAge(2);
+//		user2.setMan(false);
+//		user2.setName("Ds");
+//		
+//		List<User> users = new ArrayList<User>();
+//		users.add(user);
+//		users.add(user2);
+//
+//		String jsonString = toJSONString(users);
+//		System.out.println(jsonString);
+//
+//		List<User> parseArray = parseArray(jsonString, User.class);
+//
+//		System.out.println(toJSONString(parseArray));
+//		
+//		int[] a= {1,2,3};
+//		jsonString = toJSONString(a);
+//		System.out.println(jsonString);
+//		
+//		List<Integer> parseArray2 = parseArray(jsonString, Integer.class);
+//		System.out.println(toJSONString(parseArray2));
+//	}
 
 	/**
 	 * 把对象转换为JSON字符串
 	 */
-	public static String getJSONString(Object object) {
+	public static String toJSONString(Object object) {
 		String jsonString = null;
 		try {
 			if (object != null) {
@@ -42,14 +73,24 @@ public class JSONUtil {
 	 * 
 	 * @param <T>
 	 */
-	public static <T> Object parseObject(String jsonString, Class<T> clazz) {
+	public static <T> Object parseObject(String json, Class<T> clazz) {
 		T result = null;
 		try {
-			result = JSON.parseObject(jsonString, clazz);
+			result = JSON.parseObject(json, clazz);
 		} catch (Exception e) {
-			LogUtil.error("json转换异常,json:" + jsonString + "  class :" + clazz.getName(), e);
+			LogUtil.error("json转换异常,json:" + json + "  class :" + clazz.getName(), e);
 		}
 		return result;
+	}
+
+	public static <T> List<T> parseArray(String json, Class<T> clazz) {
+		List<T> list = null;
+		try {
+			list = JSONArray.parseArray(json, clazz);
+		} catch (Exception e) {
+			LogUtil.error("json转换异常,json:" + json + "  class :" + clazz.getName(), e);
+		}
+		return list;
 	}
 
 	public static String getValue(String json, String filed) {
